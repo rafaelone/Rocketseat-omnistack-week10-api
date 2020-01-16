c7c99a540116da884c556e831871a181ca4f1ad7
Rafael Sergio <rafa.sergio.02@live.com>
2020-01-15 22:19:37 -0300

    removido node_modules e arrumado api

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -8,14 +8,14 @@ module.exports = {
     return res.json(devs)
   },
   async store (req, res){
-    const { github_username, techs } = req.body;
+    const { github_username, techs, latitude, longitude } = req.body;
 
     let dev = await Dev.findOne({github_username})
 
     if(!dev) {
       const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`)
 
-      const { name = login, avatar_url, bio, latitude, longitude } = apiResponse.data;
+      const { name = login, avatar_url, bio } = apiResponse.data;
     
       const techsArray = parseStringAsArray(techs)
     
@@ -23,7 +23,7 @@ module.exports = {
         type: 'Point',
         coordinates:[longitude, latitude]
       }
-    
+
       dev = await Dev.create({
         github_username,
         name,
--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
-const mongoose = require('mongoose')
-const routes = require('./routes')
+const mongoose = require('mongoose');
+const routes = require('./routes');
+const cors = require('cors')
 
 const app = express();
 
-mongoose.connect('mongodb+srv://rafaelone:omnistack@cluster0-oypfw.mongodb.net/week10?retryWrites=true&w=majority', {
+
+mongoose.connect('mongodb://rafaelone:omnistack@cluster0-shard-00-00-oypfw.mongodb.net:27017,cluster0-shard-00-01-oypfw.mongodb.net:27017,cluster0-shard-00-02-oypfw.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority', {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
 
+app.use(cors())
 app.use(express.json())
 app.use(routes);
 
